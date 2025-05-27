1187b1455d24d0580bf935c85eb39a9dccd91630
Petsku01 <79454252+Petsku01@users.noreply.github.com>
2025-05-27 20:42:51 +0300

    Update SQLCA.cbl

--- SQLCA.cbl
+++ SQLCA.cbl
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
-       COBOL VERSION 2014
+       COBOL VERSION 2014.
        PROGRAM-ID. CybersecurityManagement.
       * Program to manage user login and track security incidents.
       * Compatible with COBOL 2014 (ISO/IEC 1989:2014).
